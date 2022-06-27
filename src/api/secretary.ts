e9bc5ea55c80d131afb166fd52e02805bf8bf1cb
LeoMster <37512653@qq.com>
2022-06-27 14:55:52 +0800

    feat: secretary course manage

--- src/api/secretary.ts
+++ src/api/secretary.ts
@@ -4,3 +4,9 @@ import axios from 'axios'
 export const getStudentMsgList = (id: number) => axios.get(`/api/secretary/getStudentMsg?id=${id}`)
 /** 点击查看选课信息 */
 export const checkCourseList = (id: string) => axios.get(`/api/secretary/getStudentCourseScore?id=${id}`)
+/** 课程管理获取课程列表 */
+export const getCourseList = () => axios.get(`/api/secretary/getCourseMsg`)
+/** 查看选课学生 */
+export const getStudentList = (id: string) => axios.get(`/api/secretary/getCourseStudentMsg?id=${id}`)
+/** 删除课程 */
+export const deleteCourse = (id: string) => axios.get(`/api/secretary/deleteCourse?id=${id}`)
--- src/views/secretary/SecretaryCourseManage.vue
+++ src/views/secretary/SecretaryCourseManage.vue
@@ -1,22 +1,84 @@
 <script setup lang="ts">
-import { ref } from 'vue'
+import { ref, nextTick, onMounted } from 'vue'
 import { ElMessage, ElMessageBox } from 'element-plus'
-import type { Action } from 'element-plus'
+import { getCourseList, getStudentList, deleteCourse } from '@/api/secretary'
+
+interface Course {
+  courseId: string
+  courseName: string
+  courseTime: number
+  courseResult: number
+  courseTerm: number
+  courseTeacher: number
+  studentNumber: number
+}
+
+interface Student {
+  studentId: string
+  studentName: string
+  professionId: number
+}
 
 const isVisible = ref(false)
-const tableData = [{}]
+const tableData = ref<Course[]>([])
+const courseListData = ref<Student[]>([])
 
-const deleteCourseList = () => {
+/** 课程列表 */
+const requestCourseList = async () => {
+  try {
+    const { data: res } = await getCourseList()
+    const { code, data } = res
+    if (code === 200) {
+      tableData.value = data
+    }
+  } catch (error) {
+    console.log(error)
+  }
+}
+/** 选课学生列表 */
+const requestCourseStudentList = async (id: string) => {
+  try {
+    const { data: res } = await getStudentList(id)
+    const { code, data } = res
+    if (code === 200) {
+      courseListData.value = data
+    }
+  } catch (error) {
+    console.log(error)
+  }
+}
+/** 删除课程 */
+const requestDeleteCourse = async (id: string) => {
+  try {
+    const { data: res } = await deleteCourse(id)
+    const { code, msg } = res
+    if (code === 200) {
+      ElMessage({
+        type: 'success',
+        message: '删除成功'
+      })
+    }
+    if (code === 400) {
+      ElMessage({
+        type: 'error',
+        message: msg
+      })
+    }
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+onMounted(() => requestCourseList())
+
+const deleteCourseList = (id: string) => {
   ElMessageBox.confirm('确定要删除该数据吗?', '', {
     confirmButtonText: '确定',
     cancelButtonText: '取消',
     type: 'warning'
   })
     .then(() => {
-      ElMessage({
-        type: 'success',
-        message: '删除成功'
-      })
+      requestDeleteCourse(id)
     })
     .catch(() => {
       ElMessage({
@@ -26,37 +88,57 @@ const deleteCourseList = () => {
     })
 }
 
-const checkStudentList = () => {
+const checkStudentList = async (id: string) => {
   isVisible.value = !isVisible.value
+  await nextTick()
+  requestCourseStudentList(id)
 }
 </script>
 
 <template>
   <el-table class="secretary-class-table" ref="tableRef" :data="tableData">
-    <el-table-column property="classId" label="课程号" align="center" />
-    <el-table-column property="className" label="课程名称" align="center" />
-    <el-table-column property="professionName" label="学时" align="center" />
-    <el-table-column property="compulsory" label="学分分" align="center" />
-    <el-table-column property="term" label="学期" align="center" />
-    <el-table-column property="classTeacher" label="任课教师" align="center" />
-    <el-table-column property="elective" label="选课人数" align="center" />
+    <el-table-column property="courseId" label="课程号" align="center" />
+    <el-table-column property="courseName" label="课程名称" align="center" />
+    <el-table-column property="courseTime" label="学时" align="center" />
+    <el-table-column property="courseResult" label="学分" align="center" />
+    <el-table-column property="courseTerm" label="学期" align="center" />
+    <el-table-column property="courseTeacher" label="任课教师" align="center" />
+    <el-table-column property="studentNumber" label="选课人数" align="center" />
     <el-table-column label="操作">
-      <template #default>
-        <el-button link type="primary" @click="deleteCourseList">
+      <template #default="scope">
+        <el-button
+          link
+          type="primary"
+          @click="deleteCourseList(scope.row.courseId)"
+        >
           删除
         </el-button>
-        <el-button link type="primary" @click="checkStudentList">
+        <el-button
+          link
+          type="primary"
+          @click="checkStudentList(scope.row.courseId)"
+        >
           查看
         </el-button>
       </template>
     </el-table-column>
   </el-table>
   <el-dialog v-model="isVisible" title="学生信息">
-    <el-table :data="[{}]">
+    <el-table :data="courseListData">
       <el-table-column type="index" label="序号" width="100" align="center" />
       <el-table-column property="studentId" label="学号" align="center" />
       <el-table-column property="studentName" label="姓名" align="center" />
-      <el-table-column property="profession" label="专业" align="center" />
+      <el-table-column property="professionId" label="专业" align="center">
+        <template #default="scope">
+          {{
+            !scope.row.professionId
+              ? '电子信息'
+              : scope.row.professionId === 1
+              ? '人工智能'
+              : '计算机技术'
+          }}
+        </template>
+      </el-table-column>
     </el-table>
   </el-dialog>
 </template>
